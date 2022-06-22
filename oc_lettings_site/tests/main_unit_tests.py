77d29bc286d643197f5c826e003e3fb84bc83a39
Guillaume Fauvel <89651835+guillaumefauvel@users.noreply.github.com>
2022-06-22 11:54:43 +0200

    Ajout de tests pour les units tests

--- oc_lettings_site/tests/main_unit_tests.py
+++ oc_lettings_site/tests/main_unit_tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
 from django.test import Client
 from django.contrib.auth.models import User
-from django.urls import reverse
+from django.urls import reverse, resolve
 
 from oc_lettings_site.models import Address
+from oc_lettings_site.views import index
+import lettings.views
+import profiles.views
 from profiles.models import Profile
 from lettings.models import Letting
 
@@ -35,26 +38,40 @@ class UrlTester(TestCase):
 
     def test_index(self):
         response = self.c.get(reverse('index'))
-        print(reverse('index'))
+        resolved_url = resolve(reverse('index'))
+        
+        assert resolved_url.func == index
         assert response.status_code == 200
         assert "<title>Holiday Homes</title>" in str(response.content)
 
     def test_profile_index(self):
         response = self.c.get(reverse('profiles_index'))
+        resolved_url = resolve(reverse('profiles_index'))
+        
+        assert resolved_url.func == profiles.views.index
         assert response.status_code == 200
         assert "<title>Profiles</title>" in str(response.content)
 
     def test_profile_object(self):
         response = self.c.get(reverse('profile', args=['Josh']))
+        resolved_url = resolve(reverse('profile', args=['Josh']))
+        
+        assert resolved_url.func == profiles.views.profile
         assert "<title>Josh</title>" in str(response.content)
         assert response.status_code == 200
 
     def test_letting_index(self):
         response = self.c.get(reverse('lettings_index'))
+        resolved_url = resolve(reverse('lettings_index'))
+        
+        assert resolved_url.func == lettings.views.index        
         assert response.status_code == 200
         assert "<title>Lettings</title>" in str(response.content)
 
     def test_letting_object(self):
         response = self.c.get(reverse('letting', args=[1]))
+        resolved_url = resolve(reverse('letting', args=[1]))
+                
+        assert resolved_url.func == lettings.views.letting        
         assert "<title>London Paradise</title>" in str(response.content)
         assert response.status_code == 200
