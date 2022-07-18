217d2cfa04f504197ba494afa232de6b52db02ba
Guillaume Fauvel <guillaume.fauvelpro@gmail.com>
2022-07-18 10:31:13 +0200

    DSN en variable d'environnement

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = (
 # Sentry Configuration
 
 sentry_sdk.init(
-    dsn=os.getenv('DSN_LINK', default=None),
+    dsn=os.environ['DSN_LINK'],
     integrations=[
         DjangoIntegration(
             transaction_style='url',
@@ -139,6 +139,6 @@ sentry_sdk.init(
     traces_sample_rate=1.0,
     send_default_pii=True,
     environment="debugging",
-    release=os.getenv('CIRCLE_SHA1', default=None),
+    release=os.environ['CIRCLE_SHA1'],
 )
 
